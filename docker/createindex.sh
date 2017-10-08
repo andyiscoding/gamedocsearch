6556d293af7f39662359c2d2076bf5f548f2914f
andyiscoding <andy.naylor.yes@gmail.com>
2017-10-07 21:00:28 -0400

    Debating on using kibana due to system resources being toight

---
+++ docker/createindex.sh
@@ -0,0 +1,11 @@
+curl -XPUT 'localhost:9200/data-now?pretty' -H 'Content-Type: application/json' -d'
+{
+    "settings" : {
+        "index" : {
+            "number_of_shards" : 3, 
+            "number_of_replicas" : 2 
+        }
+    }
+}
+'
+
